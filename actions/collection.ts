ba703a264ae4984b2d6613704376b700673acee3
Chameison Araújo <chameison.acre@gmail.com>
2023-09-04 20:28:32 -0500

    removendo collections

--- actions/collection.ts
+++ actions/collection.ts
@@ -1,6 +1,7 @@
-'use server'
+"use server";
 
 import prisma from "@/lib/prisma";
+import { wait } from "@/lib/wait";
 import { createCollectionSchemaType } from "@/schema/createCollection";
 import { currentUser } from "@clerk/nextjs";
 
@@ -11,12 +12,23 @@ export async function createCollection(form: createCollectionSchemaType) {
     throw new Error("user not found");
   }
 
-
   return await prisma.collection.create({
     data: {
       userId: user.id,
       color: form.color,
-      name: form.name
-    }
-  })
+      name: form.name,
+    },
+  });
+}
+
+export async function deleteCollection(id: number) {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error("Usuario não encontrado!");
+  }
+  return await prisma.collection.delete({
+    where: {
+      id: id,
+    },
+  });
 }
--- components/CollectionCard/index.tsx
+++ components/CollectionCard/index.tsx
@@ -1,17 +1,63 @@
 "use client";
 import { Collection } from "@prisma/client";
-import React, { useState } from "react";
-import { Collapsible, CollapsibleTrigger } from "../ui/collapsible";
+import React, { useState, useTransition } from "react";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "../ui/collapsible";
 import { Button } from "../ui/button";
 import { cn } from "@/lib/utils";
 import { CollectionColor, CollectionColors } from "@/lib/constants";
-import { CaretDownIcon, CaretUpIcon } from "@radix-ui/react-icons";
+import {
+  CaretDownIcon,
+  CaretUpIcon,
+  PlusCircledIcon,
+  TrashIcon,
+} from "@radix-ui/react-icons";
+import { Progress } from "../ui/progress";
+import { Separator } from "../ui/separator";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "../ui/alert-dialog";
+import { deleteCollection } from "@/actions/collection";
+import { toast } from "../ui/use-toast";
+import { useRouter } from "next/navigation";
 
 interface CollectionCardProps {
   collection: Collection;
 }
+
+const tasks: string[] = ["Tarefa1", "Tarefa2", "Tarefa3", "Tarefa4"];
+
 function CollectionCard({ collection }: CollectionCardProps) {
   const [isOpen, setIsOpen] = useState(true);
+  const router = useRouter();
+
+  const [isLoading, startTransition] = useTransition();
+  const removeCollection = async () => {
+    try {
+      await deleteCollection(collection.id);
+      toast({
+        title: "Success",
+        description: "Coleção deletada com sucesso!",
+      });
+      router.refresh();
+    } catch (e) {
+      toast({
+        title: "Error",
+        description: "Erro, Coleção não deletada",
+        variant: "destructive",
+      });
+    }
+  };
   return (
     <Collapsible open={isOpen} onOpenChange={setIsOpen}>
       <CollapsibleTrigger asChild>
@@ -19,14 +65,64 @@ function CollectionCard({ collection }: CollectionCardProps) {
           variant={"ghost"}
           className={cn(
             "flex w-full justify-between p-6",
+            isOpen && "rounded-b-none",
             CollectionColors[collection.color as CollectionColor]
           )}
         >
           <span className="text-white font-bold">{collection.name}</span>
-          {!isOpen && <CaretDownIcon className="h-6 w-6"/>}
-          {isOpen && <CaretUpIcon className="h-6 w-6"/>}
+          {!isOpen && <CaretDownIcon className="h-6 w-6" />}
+          {isOpen && <CaretUpIcon className="h-6 w-6" />}
         </Button>
       </CollapsibleTrigger>
+      <CollapsibleContent className="flex rounded-b-md flex-col dark:bg-neutral-900 shadow-lg">
+        {tasks.length === 0 && <div>Não há tarefas</div>}
+        {tasks.length > 0 && (
+          <>
+            <Progress className="rounded-none" value={75} />
+            <div className="p-4 gap-3 flex flex-col">
+              {tasks.map((task) => (
+                <div>Div das task</div>
+              ))}
+            </div>
+          </>
+        )}
+        <Separator />
+        <footer className="h-[40px] px-4 p-[2px] text-xs text-neutral-500 flex justify-between items-center ">
+          <p>Criado em {collection.createdAt.toLocaleDateString("pt-BR")}</p>
+          {isLoading && <div>Deletando...</div>}
+          {!isLoading && (
+            <div>
+              <Button size={"icon"} variant={"ghost"}>
+                <PlusCircledIcon />
+              </Button>
+              <AlertDialog>
+                <AlertDialogTrigger asChild>
+                  <Button size={"icon"} variant={"ghost"}>
+                    <TrashIcon />
+                  </Button>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                  <AlertDialogTitle>Voce tem certeza ?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    Essa ação não há volta, essa ação excluira sua coleção e
+                    todas as tarefas.
+                  </AlertDialogDescription>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel>Cancelar</AlertDialogCancel>
+                    <AlertDialogAction
+                      onClick={() => {
+                        startTransition(removeCollection);
+                      }}
+                    >
+                      Apagar
+                    </AlertDialogAction>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
+            </div>
+          )}
+        </footer>
+      </CollapsibleContent>
     </Collapsible>
   );
 }
